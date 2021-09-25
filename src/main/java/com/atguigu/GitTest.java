b2f0bdc91365ab686ea21f98ef8521f628237d12
renshaw <renshaw882@163.com>
2021-09-25 17:01:46 +0800

    git first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author Renshaw
+ * @date 2021/9/25 17:00
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
